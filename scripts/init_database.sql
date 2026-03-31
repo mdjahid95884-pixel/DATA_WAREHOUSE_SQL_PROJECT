7695fe1ba61cadeabf1efc45cc00d285d966b0ab
Mohd Jahid <mdjahid95884@gmail.com>
2026-03-31 21:19:50 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,21 @@
+--=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*--=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
+-- <WELCOME TO MY SQL DATA_WAREHOUSE_PROJECT AS A BEGINEER LEARNER THIS IS MY FIRST DATA_WAREHOUSE_PROJECT>.
+--=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*--=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*
 
+
+--<CREATING DATABSE>
+
+CREATE DATABASE DATAWAREHOUSE;
+USE DATAWAREHOUSE;
+
+--<CREATING SCHEMA FOR ARE DATAWAREHOUSE>
+
+CREATE SCHEMA BRONZE;  /*<CREATING BRONZE SCHEMA> */
+
+GO
+
+CREATE SCHEMA SILVER;  /* <CREATING BRONZE SCHEMA> */
+
+GO
+
+CREATE SCHEMA GOLDEN;  /* <CREATING BRONZE SCHEMA> */
